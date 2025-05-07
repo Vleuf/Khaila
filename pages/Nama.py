8e46d6f210715ebadd8adec936acb4420fe0ba60
Vleuf <ohkelasonline@gmail.com>
2025-05-07 11:25:54 +0700

    Update Nama.py

--- pages/Nama.py
+++ pages/Nama.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="KETUA OKPK")
 
-st.title("Database Bahan Pangan")
+st.title("Next KETUA OKPK!!")
 
 st.markdown(""" Khaila Syahira Harpil
 """)

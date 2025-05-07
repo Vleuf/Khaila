9ac1df5a60cfd8417887cb88995e2ed0e356090a
Vleuf <ohkelasonline@gmail.com>
2025-05-07 11:37:55 +0700

    Update Nama.py

--- pages/Nama.py
+++ pages/Nama.py
@@ -4,5 +4,6 @@ st.set_page_config(page_title="KETUA OKPK")
 
 st.title("Next KETUA OKPK!!")
 
-st.markdown(""" Khaila Syahira Harpil
+st.markdown(""" Khaila Syahira Harpil SIAP DAAPET KIMDAS A
+NGATAIN NJIR
 """)
